d9ed942800bda4c9b0d7b752b2268f981d1635e4
Thanh Tuấn Hoàng <57681675+hoangthanhtuanit@users.noreply.github.com>
2020-09-07 02:57:26 +0700

    update

--- backend/controllers/BannerController.php
+++ backend/controllers/BannerController.php
@@ -6,11 +6,43 @@ require_once 'helpers/Helper.php';
 class BannerController extends Controller
 {
     public function index(){
-
+        $bannerModel = new Banner();
+        $banners = $bannerModel->index();
+        $this->title_page = 'Danh sách banner';
+        $this->content = $this->render('views/banners/index.php', [
+            'banners' => $banners
+        ]);
+        require_once 'views/layouts/main.php';
     }
 
     public function create(){
+        if (isset($_POST['submit'])) {
+            $title = $_POST['title'];
+            $summary = $_POST['summary'];
+            $status = $_POST['status'];
+            if (empty($title)) {
+                $this->error = 'Tiêu đề không được để trống';
+            } elseif (empty($summary)) {
+                $this->error = 'Mô tả ngắn không được để trống';
+            } elseif ($_FILES['image']['error'] == 0) {
+                $extension = pathinfo($_FILES['image']['name'], PATHINFO_EXTENSION);
+                $extension = strtolower($extension);
+                $arrExtension = ['jpg', 'png', 'jpeg', 'gif'];
+                $fileSize = $_FILES['image']['size'] / 1024 / 1024;
+                $fileSize = round($fileSize, 2);
+                if (!in_array($extension, $arrExtension)) {
+                    $this->error = 'Cần upload file định dạng ảnh';
+                } elseif ($fileSize > 3) {
+                    $this->error = 'File không được quá 3MB';
+                }
+            }
+            if (empty($this->error)) {
+                $filename = '';
+                if ($_FILES['image']['error'] == 0) {
 
+                }
+            }
+        }
     }
 
     public function update(){
